a6649285aedc40e3183410a4ea0c2382a383765a
Maeve Griffin <griffinmaeve@gmail.com>
2019-09-05 11:55:23 -0700

    added gradient

--- public/src/components/app/UserApp.js
+++ public/src/components/app/UserApp.js
@@ -1,6 +1,21 @@
 import Component from '../Component.js';
+import { getColorAPI, toScheme } from '../../services/color-api.js';
+import { randomColor } from '../game/randomize-location.js';
 
 class UserApp extends Component {
+    onRender() {
+        const backgroundGradient = document.querySelector('html');
+        function loadGradient() {
+            const randomRBG = randomColor();
+            getColorAPI(randomRBG, 6)
+                .then(rawData => {
+                    const colorArray = toScheme(rawData);
+                    backgroundGradient.style = `background: -webkit-linear-gradient(180deg, ${colorArray[0].color} 0%, ${colorArray[5].color} 100%);`;
+        
+                });
+        }
+        loadGradient();
+    }
     renderHTML() {
         return /*html*/`
             
