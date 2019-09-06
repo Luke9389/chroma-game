3f2420fa81a3e666e9b6f7b5b06c9cba2e325e0c
Luke Donahue <luke9389@gmail.com>
2019-09-06 13:12:06 -0700

    increased number of squares to 6-8

--- public/src/components/app/GameApp.js
+++ public/src/components/app/GameApp.js
@@ -17,7 +17,7 @@ class GameApp extends Component {
         dom.appendChild(gameplay.renderDOM());
 
         function nextRound() {
-            const numOfColors = randomWholeNum(3) + 5;
+            const numOfColors = randomWholeNum(3) + 6;
             const ranColor = randomColor();
             getColorAPI(ranColor, numOfColors)
                 .then(rawData => {
